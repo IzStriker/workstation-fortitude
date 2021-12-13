acc7e8fd57c64eaf02c2d53fe0508e06f12f631e
i.d.george <i.d.george@hotmail.com>
2021-12-13 15:17:43 +0000

    FEAT: added functionality to clone vitual machines

--- vmmanagement.py
+++ vmmanagement.py
@@ -4,13 +4,14 @@ import requests
 import json
 import os
 
+base_url = "http://127.0.0.1:8697"
 
 def get_virtual_machines(credentials: str) -> str:
     headers = {
         "Accept" : "application/vnd.vmware.vmw.rest-v1+json",
         "Authorization" : "Basic " + credentials
     }
-    res = requests.get("http://127.0.0.1:8697/api/vms", headers=headers)
+    res = requests.get(base_url + "/api/vms", headers=headers)
     return json.loads(res.text)
 
 def get_vm_id(name: str, credentials: str) -> str:
@@ -19,3 +20,19 @@ def get_vm_id(name: str, credentials: str) -> str:
         if name == os.path.splitext(base)[0]:
             return vm["id"]
     raise VMNotFound(name)
+
+def clone_vm(name: str, parent_id: str, credentials: str) -> str:
+    headers = {
+      "Content-Type" : "application/vnd.vmware.vmw.rest-v1+json",
+      "Accept" : "application/vnd.vmware.vmw.rest-v1+json",
+      "Authorization" : "Basic " + credentials
+    }
+
+    body = {
+        "name" : name,
+        "parentId" : parent_id
+    }
+
+    res = requests.post(base_url + "/api/vms", headers=headers, json=body)
+    
+    return json.loads(res.text)
